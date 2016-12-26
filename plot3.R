1ec6f7c8cea99ae7035a60e2739e111073accdb9
CourseraUser2016 <gaurav.sapre1@gmail.com>
2016-12-26 20:43:46 +1100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,14 +1,5 @@
-setwd("~/Desktop/ExploratoryDataAnalysis/ExData_Plotting1")
-downloadURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-downloadFile <- "./Data/household_power_consumption.zip"
-householdFile <- "./Data/household_power_consumption.txt"
-##
-if (!file.exists(householdFile)) {
-  download.file(downloadURL, downloadFile, method = "curl")
-  unzip(downloadFile, overwrite = T, exdir = "./Data")
-}
-
-rawdata<-read.table(householdFile, header = TRUE, sep = ";", na.strings = "?")
+##SET YOUR OWN WORKING DIRECTORY WITH THE DATAFILE IN THERE 
+rawdata<-read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 dates<-strptime(rawdata$Date, "%d/%m/%Y")
 
 subsetted_data<-rawdata[dates %in%strptime(c("01/02/2007","02/02/2007"),"%d/%m/%Y") ,]
@@ -16,6 +7,7 @@ subsetted_data<-rawdata[dates %in%strptime(c("01/02/2007","02/02/2007"),"%d/%m/%
 SetTime <-strptime(paste(subsetted_data$Date, subsetted_data$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
 
 subsetted_data <- cbind(SetTime, subsetted_data)
+png(filename = "plot3.png", width = 480, height = 480, units="px")
 
 columnlines <- c("black", "red", "blue")
 labels <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
@@ -23,3 +15,6 @@ plot(subsetted_data$SetTime, subsetted_data$Sub_metering_1, type="l", col=column
 lines(subsetted_data$SetTime, subsetted_data$Sub_metering_2, col=columnlines[2])
 lines(subsetted_data$SetTime, subsetted_data$Sub_metering_3, col=columnlines[3])
 legend("topright", legend=labels, col=columnlines, lty="solid")
+
+dev.off()
+
