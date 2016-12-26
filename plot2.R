88b90c9af3bcdc22889026dbfacbd2e4c14e298b
CourseraUser2016 <gaurav.sapre1@gmail.com>
2016-12-26 20:42:19 +1100

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,13 +1,4 @@
-setwd("~/Desktop/ExploratoryDataAnalysis/ExData_Plotting1")
-downloadURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-downloadFile <- "./Data/household_power_consumption.zip"
-householdFile <- "./Data/household_power_consumption.txt"
-##
-if (!file.exists(householdFile)) {
-  download.file(downloadURL, downloadFile, method = "curl")
-  unzip(downloadFile, overwrite = T, exdir = "./Data")
-}
-
+##SET YOUR OWN WORKING DIRECTORY WITH THE DATAFILE IN THERE 
 rawdata<-read.table(householdFile, header = TRUE, sep = ";", na.strings = "?")
 dates<-strptime(rawdata$Date, "%d/%m/%Y")
 
@@ -17,4 +8,7 @@ SetTime <-strptime(paste(subsetted_data$Date, subsetted_data$Time, sep=" "),"%d/
 
 subsetted_data <- cbind(SetTime, subsetted_data)
 
+#plot2
+png(filename = "plot2.png", width = 480, height = 480, units = "px")
 plot(subsetted_data$SetTime, subsetted_data$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
